71eb7d925332611521f8c289ca02abaf29d73a14
Shubham Singh <64443633+ShubhamSingh047@users.noreply.github.com>
2024-03-27 01:10:55 +0530

    Delete src directory
    invalid directory due to rebasing

--- src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import styles from "./Logo.module.css";
-import logo from "../assets/logo.png";
-
-function Logo() {
-  return <img src={logo} alt="WorldWise logo" className={styles.logo} />;
-}
-
-export default Logo;
--- src/components/Map.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Map.module.css";
-
-const Map = () => {
-  return <div className={styles.mapContainer}>Map</div>;
-};
-
-export default Map;
